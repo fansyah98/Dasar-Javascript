0d47f22d845a8105e8383df9176f3e8b47c4291d
fansyah98 <fansyahdwi204@gmail.com>
2022-08-22 22:51:19 +0700

    Dasar Javascript

---
+++ 2.js
@@ -0,0 +1,15 @@
+var firstName = "Fansyah Dwi ";
+var lastName = "Krisnady"
+var age = 20
+const isMarried = false
+
+console.log(isMarried);
+// TODO
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+
+module.exports = {
+  firstName, lastName, age, isMarried,
+};
